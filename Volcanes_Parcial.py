4e14c54489ebd1c234d3e07c10e321b74625d540
Javiera-HS <javier.herreras@duocuc.cl>
2025-07-03 13:26:21 -0400

    cambio en salida del programa

--- Volcanes_Parcial.py
+++ Volcanes_Parcial.py
@@ -87,7 +87,7 @@ while True:
     elif opc=='5':
         mostrar()
     elif opc=='6':
-        print('Hasta la vista Baby.....')
+        print('Ha salido exitosamente del programa.')
         break
     else:
         print('Opción Incorrecta')
